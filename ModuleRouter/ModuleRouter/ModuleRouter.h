f5d44296e0344c88619c56409b7c3cb26c4f9aaa
apple <apple@appledeMacBook-Pro.local>
2019-03-15 09:32:57 +0800

    xxx

---
+++ ModuleRouter/ModuleRouter/ModuleRouter.h
@@ -0,0 +1,19 @@
+//
+//  ModuleRouter.h
+//  ModuleRouter
+//
+//  Created by apple on 2019/3/15.
+//  Copyright © 2019年 zsx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ModuleRouter.
+FOUNDATION_EXPORT double ModuleRouterVersionNumber;
+
+//! Project version string for ModuleRouter.
+FOUNDATION_EXPORT const unsigned char ModuleRouterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ModuleRouter/PublicHeader.h>
+
+
